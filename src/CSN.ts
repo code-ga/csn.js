99a33be37692dafc2dd8f0f655c4682b89772fea
CookieGMVN <tuyentrandinh00@gmail.com>
2023-01-20 08:50:39 +0700

    (fix) Fixed export.

--- src/CSN.ts
+++ src/CSN.ts
@@ -1,8 +1,3 @@
 import Client from "./Client";
-import Music from "./Classes/Music";
-import Playlist from "./Classes/Playlist";
-import Search from "./Classes/Search";
-import TopChart from "./Classes/TopChart";
-import User from "./Classes/User";
 
-export { Client, Music, Playlist, Search, TopChart, User }
+module.exports = Client;
