75892e41782577aa239e297e7f01f30e1287f7c9
code-ga <tritranduc11@gmail.com>
2023-01-26 13:52:06 +0700

    delete dist

--- dist/CSN.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Client_1 = __importDefault(require("./Client"));
-module.exports = Client_1.default;
--- dist/Typings/Classes/HTTPError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class HTTPError extends Error {
-    constructor(message) {
-        super(message);
-        this.name = "HTTPError";
-    }
-}
-exports.default = HTTPError;
